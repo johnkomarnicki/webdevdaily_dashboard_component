934e4476538c85d2fc548a5d9e2cee5665ddb2e8
John <john@johnkomarnicki.com>
2024-06-21 16:27:19 -0400

    fix: add starting font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "Jakarta-Sans": ["Plus Jakarta Sans, sans-serif"],
+    },
   },
   plugins: [],
 };
